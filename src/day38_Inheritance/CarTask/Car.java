f9da891b37f52fa9094e7822a08f191016fab653
mcengiz <cengizulu@gmail.com>
2022-04-12 23:09:17 +0200

    day38:Inheritance- Method Overriding

---
+++ src/day38_Inheritance/CarTask/Car.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.CarTask;
+
+public class Car {
+    public String brand;
+    public String model;
+    public int year;
+    public double price;
+    public String color;
+
+
+
+    
+}
