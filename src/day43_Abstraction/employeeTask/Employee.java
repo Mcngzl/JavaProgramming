2eefbc80ae038261b234e899b2b012d5f7d9f3a6
mcengiz <cengizulu@gmail.com>
2022-04-29 02:51:53 +0200

    day 43 abstraction

---
+++ src/day43_Abstraction/employeeTask/Employee.java
@@ -0,0 +1,47 @@
+package day43_Abstraction.employeeTask;
+
+public abstract class Employee extends Person{
+
+    private final int id;
+    private String jobtitle;
+    private double salary;
+
+    public Employee(String name, int age, char gender, int id, String jobtitle, double slary) {
+        super(name, age, gender);
+        this.id = id;
+        this.jobtitle = jobtitle;
+        this.salary = slary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getJobtitle() {
+        return jobtitle;
+    }
+
+    public void setJobtitle(String jobtitle) {
+        this.jobtitle = jobtitle;
+    }
+
+    public double getSlary() {
+        return salary;
+    }
+
+    public void setSlary(double slary) {
+        this.salary = slary;
+    }
+
+    public abstract void work();
+
+    public String toString() {
+        return "Employee{" +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                ", id=" + id +
+                ", jobtitle='" + jobtitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
